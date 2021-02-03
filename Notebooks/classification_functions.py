0407e7b22ce90f92b1935a9acb340e38eb7d3f93
Rachel Dilley <racheldilley11@gamil.com>
2021-02-02 20:24:52 -0500

    add log loss

--- Notebooks/classification_functions.py
+++ Notebooks/classification_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #modeling
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import roc_auc_score, confusion_matrix, roc_curve, precision_score, recall_score, f1_score, fbeta_score, auc
+from sklearn.metrics import roc_auc_score, confusion_matrix, roc_curve, precision_score, recall_score, f1_score, fbeta_score, auc, log_loss
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
@@ -16,7 +16,7 @@ def logistic_model(X_train, y_train, regularization, threshold, threshold_val, b
     #this helps with the way kf will generate indices below
     X, y = np.array(X_train), np.array(y_train)
     kf = KFold(n_splits=5, shuffle=True, random_state=23) #randomly shuffle before splitting
-    precision, recall, log_score, f1, fbeta, auc = [] , [], [], [], [], []
+    precision, recall, f1, fbeta, auc, logl = [] , [], [], [], [], []
 
     for train_ind, val_ind in kf.split(X, y):
         X_train, y_train = X[train_ind], y[train_ind]
@@ -30,7 +30,7 @@ def logistic_model(X_train, y_train, regularization, threshold, threshold_val, b
         else:
             preds = lm.predict(X_val)
     
-        log_score.append(round(lm.score( X_val, y_val), 3))
+        logl.append(round(log_loss( y_val, preds), 3))
         precision.append(round(precision_score( y_val, preds, average='macro'), 3))
         recall.append(round(recall_score( y_val, preds, average='macro'), 3))
         f1.append(round(f1_score( y_val, preds, average='macro'), 3))
@@ -41,12 +41,12 @@ def logistic_model(X_train, y_train, regularization, threshold, threshold_val, b
         auc.append(round(roc_auc_score( y_val_enc, preds_enc, average='macro', multi_class='ovr'), 3))
 
     print(f'logistic regression with C = {regularization}:\n'
-          f'Logistic score: {np.mean(log_score)},\n'
           f'Precision score: {np.mean(precision)},\n'
           f'Recall score: {np.mean(recall)},\n'
           f'f1 score: {np.mean(f1)},\n'
           f'fbeta score for beta = {b}: {np.mean(fbeta)},\n'
-          f'ROC AUC score: {np.mean(auc)},\n')
+          f'ROC AUC score: {np.mean(auc)},\n'
+          f'Log-loss: {np.mean(logl)},\n')
     plot_roc(y_val, preds)
           
     return lm
@@ -55,7 +55,7 @@ def knn_classification(X_train, y_train, k, b):
     #this helps with the way kf will generate indices below
     X, y = np.array(X_train), np.array(y_train)
     kf = KFold(n_splits=5, shuffle=True, random_state=23) #randomly shuffle before splitting
-    precision, recall, f1, fbeta, auc = [] , [], [], [], []
+    precision, recall, f1, fbeta, auc, logl = [] , [], [], [], [], []
 
     knn = KNeighborsClassifier(n_neighbors=k) #k nearest neighbors
     knn.fit(X_train, y_train)
@@ -68,7 +68,8 @@ def knn_classification(X_train, y_train, k, b):
         recall.append(round(recall_score( y_val, knn.predict(X_val), average='macro'), 3))
         f1.append(round(f1_score( y_val, knn.predict(X_val), average='macro'), 3))
         fbeta.append(round(fbeta_score( y_val, knn.predict(X_val), beta = b, average='macro'), 3)) #beta times more impotance to recall than precision
-        
+        logl.append(round(log_loss( y_val, knn.predict(X_val)), 3))
+
         y_val_enc = pd.get_dummies(y_val)
         preds_enc = pd.get_dummies(knn.predict(X_val))
         auc.append(round(roc_auc_score( y_val_enc, preds_enc, average='macro', multi_class='ovr'), 3))
@@ -78,7 +79,8 @@ def knn_classification(X_train, y_train, k, b):
           f'Recall score: {np.mean(recall)},\n'
           f'f1 score: {np.mean(f1)},\n'
           f'fbeta score for beta = {b}: {np.mean(fbeta)},\n'
-          f'ROC AUC score: {np.mean(auc)},\n')
+          f'ROC AUC score: {np.mean(auc)},\n'
+          f'Log-loss: {np.mean(logl)},\n')
     plot_roc(y_val, knn.predict(X_val))
     
     return knn
