d4816981dd9b052c25f7406a8d88ab77ef7af59d
Rachel Dilley <racheldilley11@gamil.com>
2021-02-04 10:42:41 -0500

    apply calculate scores fucntion to all model functions

--- Notebooks/classification_functions.py
+++ Notebooks/classification_functions.py
@@ -40,7 +40,6 @@ def multinomial_nb(X_train, y_train, b):
 
     print(f'Multinomial NB:\n')
     get_scores(ac, precision, recall, f1, fbeta, b, auc, logl)
-    #plot_roc(y_val, preds)
     plot_roc(y_val, X_val, mnb)
           
     return mnb
@@ -57,22 +56,19 @@ def random_forest(X_train, y_train, estimators, b):
 
         rf = RandomForestClassifier(n_estimators=estimators)
         rf.fit(X_train, y_train)
-        preds = rf.predict(X_val)
 
-        ac.append(round(rf.score( X_val, y_val), 3))
-        precision.append(round(precision_score( y_val, preds, average='macro'), 3))
-        recall.append(round(recall_score( y_val, preds, average='macro'), 3))
-        f1.append(round(f1_score( y_val, preds, average='macro'), 3))
-        fbeta.append(round(fbeta_score( y_val, preds, beta = b, average='macro'), 3)) #beta times more impotance to recall than precision
-        
-        y_val_enc = pd.get_dummies(y_val)
-        probs = rf.predict_proba(X_val)
-        auc.append(round(roc_auc_score( y_val_enc, probs, average='macro', multi_class='ovr'), 3))
-        logl.append(round(log_loss( y_val, probs), 3))
+        metrics = calc_scores(rf, X_val, y_val, b)
+
+        ac.append(metrics[0])
+        precision.append(metrics[1])
+        recall.append(metrics[2])
+        f1.append(metrics[3])
+        fbeta.append(metrics[4]) 
+        auc.append(metrics[5])
+        logl.append(metrics[6])
 
     print(f'Random Forest with {estimators} estimators:\n')
     get_scores(ac, precision, recall, f1, fbeta, b, auc, logl)
-    #plot_roc(y_val, preds)
     plot_roc(y_val, X_val, rf)
           
     return rf
@@ -89,27 +85,24 @@ def decision_tree(X_train, y_train, depth, b):
 
         dt = DecisionTreeClassifier(max_depth=depth)
         dt.fit(X_train, y_train)
-        preds = dt.predict(X_val)
 
-        ac.append(round(dt.score( X_val, y_val), 3))
-        precision.append(round(precision_score( y_val, preds, average='macro'), 3))
-        recall.append(round(recall_score( y_val, preds, average='macro'), 3))
-        f1.append(round(f1_score( y_val, preds, average='macro'), 3))
-        fbeta.append(round(fbeta_score( y_val, preds, beta = b, average='macro'), 3)) #beta times more impotance to recall than precision
-        
-        y_val_enc = pd.get_dummies(y_val)
-        probs = dt.predict_proba(X_val)
-        auc.append(round(roc_auc_score( y_val_enc, probs, average='macro', multi_class='ovr'), 3))
-        logl.append(round(log_loss( y_val, probs), 3))
+        metrics = calc_scores(dt, X_val, y_val, b)
+
+        ac.append(metrics[0])
+        precision.append(metrics[1])
+        recall.append(metrics[2])
+        f1.append(metrics[3])
+        fbeta.append(metrics[4]) 
+        auc.append(metrics[5])
+        logl.append(metrics[6])
 
     print(f'Decision Tree with max depth of {depth}:\n')
     get_scores(ac, precision, recall, f1, fbeta, b, auc, logl)
-    #plot_roc(y_val, preds)
     plot_roc(y_val, X_val, dt)
           
     return dt
 
-def logistic_model(X_train, y_train, regularization, threshold, threshold_val, b):
+def logistic_model(X_train, y_train, regularization, b):
     #this helps with the way kf will generate indices below
     X, y = np.array(X_train), np.array(y_train)
     kf = KFold(n_splits=5, shuffle=True, random_state=23) #randomly shuffle before splitting
@@ -122,25 +115,18 @@ def logistic_model(X_train, y_train, regularization, threshold, threshold_val, b
         lm = LogisticRegression(C=regularization, max_iter=10000)
         lm.fit(X_train, y_train)
 
-        if threshold:
-            preds = (lm.predict_proba(X_val)[:, 1] >= threshold_val)
-        else:
-            preds = lm.predict(X_val)
-    
-        ac.append(round(lm.score( X_val, y_val), 3))
-        precision.append(round(precision_score( y_val, preds, average='macro'), 3))
-        recall.append(round(recall_score( y_val, preds, average='macro'), 3))
-        f1.append(round(f1_score( y_val, preds, average='macro'), 3))
-        fbeta.append(round(fbeta_score( y_val, preds, beta = b, average='macro'), 3)) #beta times more impotance to recall than precision
-        
-        y_val_enc = pd.get_dummies(y_val)
-        probs = lm.predict_proba(X_val)
-        auc.append(round(roc_auc_score( y_val_enc, probs, average='macro', multi_class='ovr'), 3))
-        logl.append(round(log_loss( y_val, probs), 3))
+        metrics = calc_scores(lm, X_val, y_val, b)
+
+        ac.append(metrics[0])
+        precision.append(metrics[1])
+        recall.append(metrics[2])
+        f1.append(metrics[3])
+        fbeta.append(metrics[4]) 
+        auc.append(metrics[5])
+        logl.append(metrics[6])
 
     print(f'logistic regression with C = {regularization}:\n')
     get_scores(ac, precision, recall, f1, fbeta, b, auc, logl)
-    #plot_roc(y_val, preds)
     plot_roc(y_val, X_val, lm)
           
     return lm
@@ -151,27 +137,25 @@ def knn_classification(X_train, y_train, k, b):
     kf = KFold(n_splits=5, shuffle=True, random_state=23) #randomly shuffle before splitting
     precision, recall, f1, fbeta, auc, logl, ac = [] , [], [], [], [], [], []
 
-    knn = KNeighborsClassifier(n_neighbors=k) #k nearest neighbors
-    knn.fit(X_train, y_train)
-
     for train_ind, val_ind in kf.split(X, y):
         X_train, y_train = X[train_ind], y[train_ind]
         X_val, y_val = X[val_ind], y[val_ind]
 
-        ac.append(round(knn.score( X_val, y_val), 3))
-        precision.append(round(precision_score( y_val, knn.predict(X_val), average='macro'), 3))
-        recall.append(round(recall_score( y_val, knn.predict(X_val), average='macro'), 3))
-        f1.append(round(f1_score( y_val, knn.predict(X_val), average='macro'), 3))
-        fbeta.append(round(fbeta_score( y_val, knn.predict(X_val), beta = b, average='macro'), 3)) #beta times more impotance to recall than precision
-        
-        y_val_enc = pd.get_dummies(y_val)
-        probs = knn.predict_proba(X_val)
-        auc.append(round(roc_auc_score( y_val_enc, probs, average='macro', multi_class='ovr'), 3))
-        logl.append(round(log_loss( y_val, probs), 3))
+        knn = KNeighborsClassifier(n_neighbors=k) #k nearest neighbors
+        knn.fit(X_train, y_train)
+
+        metrics = calc_scores(knn, X_val, y_val, b)
+
+        ac.append(metrics[0])
+        precision.append(metrics[1])
+        recall.append(metrics[2])
+        f1.append(metrics[3])
+        fbeta.append(metrics[4]) 
+        auc.append(metrics[5])
+        logl.append(metrics[6])
 
     print(f'KNN Classification with k = {k}:\n')
     get_scores(ac, precision, recall, f1, fbeta, b, auc, logl)
-    #plot_roc(y_val, knn.predict(X_val))
     plot_roc(y_val, X_val, knn)
     
     return knn
