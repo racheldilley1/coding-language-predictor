007a100aa75dd406cc23259c8c959f418fa0cf66
Rachel Dilley <racheldilley11@gamil.com>
2021-02-06 23:08:43 -0500

    add multiselect box

--- WebApp/OpSys_Classification_App.py
+++ WebApp/OpSys_Classification_App.py
@@ -4,6 +4,10 @@ import streamlit as st
 st.title('Operating System Predictor')
 #st.image()
 
+st.multiselect('Select Coding Languages', 
+                ['JavaScript', 'Python', 'SQL', 'HTML/CSS', 'Java'],
+                key = 'language')
+
 if st.button('Get Prediction', 'predict'):
 
     OpSys = 'Windows'
