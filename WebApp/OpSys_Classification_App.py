5a7c51e7f1ed729ef3b71a0d6005740296b8ebdb
Rachel Dilley <racheldilley11@gamil.com>
2021-02-06 23:43:36 -0500

    add select boxes

--- WebApp/OpSys_Classification_App.py
+++ WebApp/OpSys_Classification_App.py
@@ -14,6 +14,27 @@ st.selectbox('Select Region', ['North America',
         'Commonwealth of Independant States',
         'Africa', 'Baltics'], key = 'region')
 
+st.selectbox('Select Education Level', 
+            ['Masters/PHD',
+             'Bachelors ', 'Associates',
+            'Professional Degree', 'Some University',
+            'Secondary or Primary School', 
+            'None'], key = 'edlevel')
+
+st.selectbox('Select Occupation/Developer Type', 
+            ['Not a Developer', 'Sometimes Code at Work',
+             'Retired Developer', 'Back-End Developer', 
+             'Full-Stack Developer', 'Front-End Developer', 
+             'Desktop', 'Mobile', 'DevOps', 'Database Administrator', 
+             'Designer', 'System Administrator', 
+             'Student'], key = 'jobtype')
+
+st.selectbox('Select Undergrad Major', ['Computer Science/Engineering',
+        'Engineering', 'Information Technology/Systems',
+        'Health/Natural Sciences', 'Human/Social Sciences', 
+        'Math/Statistics', 'Web Development',
+        'Arts', 'None'], key = 'major')
+
 st.multiselect('Select Coding Languages', 
                 ['JavaScript', 'Python', 'SQL', 'HTML/CSS', 'Java'],
                 key = 'language')
