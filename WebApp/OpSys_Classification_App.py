5bd3f463e4554d04dd8ed1b38466868d192e1bf4
Rachel Dilley <racheldilley11@gamil.com>
2021-02-06 23:00:40 -0500

    add button

--- WebApp/OpSys_Classification_App.py
+++ WebApp/OpSys_Classification_App.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+
 st.title('Operating System Predictor')
+#st.image()
+
+if st.button('Get Prediction', 'predict'):
+
+    OpSys = 'Windows'
+    st.write('The Prediction is: ', OpSys)
+
